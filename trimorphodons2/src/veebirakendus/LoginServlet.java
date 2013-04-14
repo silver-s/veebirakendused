6b573d89a2b2795bf89ef4a8a08137884ad32eba
ecbyu <ecbyu7@gmail.com>
2013-04-14 22:35:19 +0300

    fb login

--- trimorphodons2/src/veebirakendus/LoginServlet.java
+++ trimorphodons2/src/veebirakendus/LoginServlet.java
@@ -1,12 +1,22 @@
 package veebirakendus;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.IOException;
+
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.google.appengine.api.users.User;
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.gson.Gson;
  
 @SuppressWarnings("serial")
 public class LoginServlet extends HttpServlet {
@@ -14,5 +24,72 @@ public class LoginServlet extends HttpServlet {
              throws IOException {
        String kood = req.getParameter("code");
        System.out.println(kood);
+       
+       String token = null;
+       try {
+           String g = "https://graph.facebook.com/oauth/access_token?client_id=354733337962261&redirect_uri=" + URLEncoder.encode("http://trimorphodons.appspot.com/login", "UTF-8") + "&client_secret=d9eddfbfd0c379b5c1e8580d55b8430f&code=" + kood;
+           URL u = new URL(g);
+           URLConnection c = u.openConnection();
+           BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
+           String inputLine;
+           StringBuffer b = new StringBuffer();
+           while ((inputLine = in.readLine()) != null)
+               b.append(inputLine + "\n");            
+           in.close();
+           token = b.toString();
+           if (token.startsWith("{"))
+               throw new Exception("error on requesting token: " + token + " with code: " + kood);
+       } catch (Exception e) {
+               // an error occurred, handle this
+       }
+
+       String graph = null;
+       try {
+           String g = "https://graph.facebook.com/me?" + token;
+           URL u = new URL(g);
+           URLConnection c = u.openConnection();
+           BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
+           String inputLine;
+           StringBuffer b = new StringBuffer();
+           while ((inputLine = in.readLine()) != null)
+               b.append(inputLine + "\n");            
+           in.close();
+           graph = b.toString();
+       } catch (Exception e) {
+               // an error occurred, handle this
+       }
+
+       String facebookId;
+       String firstName;
+       String middleNames;
+       String lastName;
+       
+       try {
+
+           JSONObject json = new JSONObject(graph);
+
+           facebookId = json.getString("id");
+
+           firstName = json.getString("first_name");
+
+           if (json.has("middle_name"))
+
+              middleNames = json.getString("middle_name");
+
+           else
+
+               middleNames = null;
+
+           if (middleNames != null && middleNames.equals(""))
+
+               middleNames = null;
+
+           lastName = json.getString("last_name");
+
+       } catch (JSONException e) {
+
+           // an error occurred, handle this
+
+       }
    }
 }
